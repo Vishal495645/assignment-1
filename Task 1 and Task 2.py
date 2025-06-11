f858678dd6119cf0b3a000c550b0b5bbf01037dd
Vishal495645 <vy535279@gmail.com>
2025-06-11 13:40:53 +0530

    Add files via upload

---
+++ Task 1 and Task 2.py
@@ -0,0 +1,18 @@
+# Task 1  Perform Basic Mathematical Operations
+# Takes two numbers as input from the user
+'''x=input('x: ' )
+y=input('y: ' )
+x=float(x)
+y=float(y)
+print('add od the numbers:',x+y)
+print('subtract od the numbers:',x-y)
+print('multiply od the numbers:',x*y)
+print('divide od the numbers:',x/y)'''
+
+#Task 2  Create a Personalized Greeting
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+full_name = first_name + " " + last_name
+
+print(f"\nHello, {full_name}! Welcome to the Python program.")
+
